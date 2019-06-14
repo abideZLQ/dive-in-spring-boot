3e481afedb923f7d321b1eb667b94d83d6de89a7
zlq <1756550688@qq.com>
2019-06-14 16:58:12 +0800

    20190614 commit study code - 今天周五，回家可以打LOL~

---
+++ spring-application/src/main/java/com/zlq/diveinspringboot/SpringApplicationContextBootstrap.java
@@ -0,0 +1,24 @@
+package com.zlq.diveinspringboot;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Spring 应用上下文 引导类
+ *
+ * @author zlq
+ * @since 2019/6/14
+ */
+@SpringBootApplication
+public class SpringApplicationContextBootstrap {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(SpringApplicationContextBootstrap.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        System.out.println("ConfigurableApplicationContext 类型：" + context.getClass().getName());
+        System.out.println("Environment 类型：" + context.getEnvironment().getClass().getName());
+        // 关闭上下文
+        context.close();
+    }
+}

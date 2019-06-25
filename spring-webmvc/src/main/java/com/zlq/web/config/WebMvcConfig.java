ba252763725d89718a06633462bbc842cf71e0c5
zlq <1756550688@qq.com>
2019-06-25 17:13:32 +0800

    20190625 commit study code - 一直在忙项目导出

---
+++ spring-webmvc/src/main/java/com/zlq/web/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.zlq.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Spring Web MVC 配置（类）
+ *
+ * @author zlq
+ * @since 2019/6/25
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+
+//     <!--<bean id="viewResolver" class="org.springframework.web.servlet.view.InternalResourceViewResolver">-->
+//        <!--<property name="viewClass" value="org.springframework.web.servlet.view.JstlView"/>-->
+//        <!--<property name="prefix" value="/WEB-INF/jsp/"/>-->
+//        <!--<property name="suffix" value=".jsp"/>-->
+//    <!--</bean>-->
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}

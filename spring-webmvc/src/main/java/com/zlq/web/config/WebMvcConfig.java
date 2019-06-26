8a268adc7c83f82135a47b6b847cad3dc245b67f
zlq <1756550688@qq.com>
2019-06-26 17:26:56 +0800

    20190626 commit study code - 学习 注解驱动@Ebable和注解驱动相关注解

--- spring-webmvc/src/main/java/com/zlq/web/config/WebMvcConfig.java
+++ spring-webmvc/src/main/java/com/zlq/web/config/WebMvcConfig.java
@@ -2,10 +2,16 @@ package com.zlq.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Spring Web MVC 配置（类）
@@ -15,7 +21,7 @@ import org.springframework.web.servlet.view.JstlView;
  */
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 //     <!--<bean id="viewResolver" class="org.springframework.web.servlet.view.InternalResourceViewResolver">-->
 //        <!--<property name="viewClass" value="org.springframework.web.servlet.view.JstlView"/>-->
@@ -30,5 +36,17 @@ public class WebMvcConfig {
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册一个拦截器,使用匿名内部类
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println("拦截中...");
+                return true;
+            }
+        });
     }
 }
--- spring-webmvc/src/main/java/com/zlq/web/controller/HelloWorldController.java
+++ spring-webmvc/src/main/java/com/zlq/web/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.zlq.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -16,8 +17,26 @@ public class HelloWorldController {
      * 映射到根路径
      */
     @RequestMapping("")
-    public String index() {
+    public String index(Model model) {
+//      model.addAttribute("acceptLanguage",acceptLanguage);
+//      model.addAttribute("jsessionId",jsessionId);
+//      model.addAttribute("message","Hello,World");
         return "index";
+    }
+
+    @ModelAttribute("message")
+    public String message(){
+        return "Hello,World";
+    }
+
+    @ModelAttribute("acceptLanguage")
+    public String acceptLanguage(@RequestHeader("Accept-Language") String acceptLanguage){
+        return acceptLanguage;
+    }
+
+    @ModelAttribute("jsessionId")
+    public String jsessionId(@CookieValue("zlq") String jsessionId){
+        return jsessionId;
     }
 
 }

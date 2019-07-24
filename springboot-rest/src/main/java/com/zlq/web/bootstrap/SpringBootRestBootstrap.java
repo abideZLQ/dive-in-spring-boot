a95f06f69b8dcb8dce86b4cea1929a3ff4ab338a
zlq <1756550688@qq.com>
2019-07-24 16:45:54 +0800

    20190724 commit study code - springboot-rest

---
+++ springboot-rest/src/main/java/com/zlq/web/bootstrap/SpringBootRestBootstrap.java
@@ -0,0 +1,21 @@
+package com.zlq.web.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Rest 引导类
+ *
+ * @author zlq
+ * @since 2019/7/22
+ */
+@SpringBootApplication(scanBasePackages = {
+        "com.zlq.web.controller"
+})
+public class SpringBootRestBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestBootstrap.class,args);
+    }
+
+}
---
+++ springboot-rest/src/main/java/com/zlq/web/controller/HelloWorldRestController.java
@@ -0,0 +1,21 @@
+package com.zlq.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloWorld {@link RestController} 实现
+ *
+ * @author zlq
+ * @since 2019/7/22
+ */
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping(value = "/hello-world")
+    public String helloWorld(@RequestParam(required = false) String message){
+        return "Hello,World! : " + message;
+    }
+
+}

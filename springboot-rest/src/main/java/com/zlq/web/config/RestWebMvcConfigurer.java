dc8087075fd31d61aacc0b9c6257b1c3c907ad06
zlq <1756550688@qq.com>
2019-08-02 17:21:48 +0800

    20190802 commit study code - springboot-rest

---
+++ springboot-rest/src/main/java/com/zlq/web/config/RestWebMvcConfigurer.java
@@ -0,0 +1,6 @@
+package com.zlq.web.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class RestWebMvcConfigurer implements WebMvcConfigurer {
+}
--- springboot-rest/src/main/java/com/zlq/web/controller/UserRestController.java
+++ springboot-rest/src/main/java/com/zlq/web/controller/UserRestController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserRestController {
 
-    @PostMapping(value = "/echo/user")
+    @PostMapping(value = "/echo/user",
+            consumes = "application/*;charset=UTF-8",
+            produces = "application/json;charset=UTF-8")
     public User user(@RequestBody User user){
         return user;
     }
